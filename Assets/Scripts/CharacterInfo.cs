2a724a682a731289f6fc25401a81f8258aaa0732
Angel Eduardo Vazquez Garcia <angel.eduardo.v.g@hotmail.com>
2021-02-14 12:58:15 -0600

    Creación de prefab de los personajes

--- Assets/Scripts/CharacterInfo.cs
+++ Assets/Scripts/CharacterInfo.cs
@@ -1,15 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterInfo : MonoBehaviour
 {
     public GameObject target;
     private RectTransform rectTransform;
     public bool isStart = false;
+    private string titleCharacter = "Character";
     void Start() {
         rectTransform = GetComponent<RectTransform>();
     }    
+    public void SetTitle(string titleCharacter) {
+        this.titleCharacter = titleCharacter;
+        foreach(Transform child in gameObject.transform) {
+            if(child.gameObject.tag == "Text") {
+                child.gameObject.GetComponent<Text>().text = this.titleCharacter;
+            }
+        }
+    }
     void Update() {
         if(isStart)
         {
--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,11 +4,32 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    public string titleEnemy = "Enemy";
+    private CharacterController controller;
+    public GameObject groupBox;
     // Start is called before the first frame update
     public float health = 100.0f;
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+        if (!GameObject.FindGameObjectWithTag("UIGame"))
+        {
+            Debug.Log("UIGame no existe");
+            return;
+        }
+
+        GameObject _groupBox = GameObject.Instantiate(groupBox, GameObject.FindGameObjectWithTag("UIGame").transform);
+        _groupBox.AddComponent<CharacterInfo>();
+        CharacterInfo ci = _groupBox.GetComponent<CharacterInfo>();
+        foreach (Transform child in gameObject.transform)
+{
+            if (child.gameObject.tag == "targetAboveUI")
+            {
+                ci.target = child.gameObject;
+            }
+        }
+        ci.isStart = true;
+        ci.SetTitle(titleEnemy);
     }
 
     // Update is called once per frame
